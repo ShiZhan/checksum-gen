74f7233a21178579460b9050dbed408cd40d5e8b
ShiZhan <g.shizhan.g@gmail.com>
2013-12-08 17:20:08 -0600

    replace java.util.zip and java.security.MessageDigest with apache commons compress and codec

--- src/main/scala/ChecksumGen4zip.scala
+++ src/main/scala/ChecksumGen4zip.scala
@@ -1,21 +1,21 @@
 object ChecksumGen4zip {
 
-  import java.io.{ File, FileInputStream }
-  import java.util.zip.{ ZipFile, ZipInputStream }
-  import util.Hash
+  import java.io.{ File, InputStream }
+  import org.apache.commons.compress.archivers.zip.ZipFile
+  import org.apache.commons.codec.digest.DigestUtils.md5Hex
 
   private def zipMD5(file: File) = {
     try {
       val zf = new ZipFile(file)
-      val zis = new ZipInputStream(new FileInputStream(file))
-      val entries =
-        Iterator.continually(zis.getNextEntry).takeWhile(null !=).filter(!_.isDirectory)
-      entries map { e =>
+      val entries = zf.getEntries
+      val files = Iterator.continually {
+        if (entries.hasMoreElements) entries.nextElement else null
+      }.takeWhile(null !=).filter(!_.isDirectory)
+      files map { e =>
         val path = e.getName
         val size = e.getSize
         val is = zf.getInputStream(e)
-        val bytes = Stream.continually(is.read).takeWhile(-1 !=).map(_.toByte)
-        val md5 = Hash.md5sum(bytes.toIterator)
+        val md5 = md5Hex(is)
         is.close
         md5Tuple(md5, path, size)
       }
@@ -26,7 +26,7 @@ object ChecksumGen4zip {
 
   def main(args: Array[String]) = {
     if (args.length < 1)
-      println("usage: ChecksumGen4zip <zip|jar|apk file (UTF-8 only)>")
+      println("usage: ChecksumGen4zip <zip|jar|apk file>")
     else
       zipMD5(new File(args(0))) foreach println
   }
