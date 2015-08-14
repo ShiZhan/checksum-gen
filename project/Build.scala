da6f6909e123d157e5f6b007caafbad4df375151
ShiZhan <g.shizhan.g@gmail.com>
2015-08-14 12:32:35 +0800

    project definition

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,36 @@
 import sbt._
 import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object MyBuild extends Build {
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-    version := "0.1-SNAPSHOT",
+  val scalaV = Option(System.getProperty("scala.version")).getOrElse("2.11.6")
+  val codecV = "1.10"
+  val comprV = "1.9"
+
+  lazy val commonSettings = Defaults.defaultSettings ++ Seq(
+    version      := "0.1-SNAPSHOT",
     organization := "com.simba",
-    scalaVersion := "2.11.6"
+    scalaVersion := scalaV,
+    scalacOptions in Compile ++= Seq(
+      "-encoding", "UTF-8",
+      "-deprecation", "-feature", "-unchecked",
+      "-Xlint"),
+    libraryDependencies ++= Seq(
+      "commons-codec"      % "commons-codec"    % codecV,
+      "org.apache.commons" % "commons-compress" % comprV
+    )
   )
+
+  lazy val buildSettings = commonSettings
 
   lazy val ChecksumGen = Project(
     id = "ChecksumGen",
     base = file("."),
-    settings = Defaults.defaultSettings ++
-    sbtassembly.Plugin.assemblySettings
+    settings = commonSettings ++
+    sbtassembly.Plugin.assemblySettings ++
+    Seq(
+      logLevel in assembly := Level.Warn
+    )
   )
 }
